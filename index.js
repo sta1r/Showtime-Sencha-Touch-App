e073aabca9057c0e6fcf33424934a70a0000c858
Chris Toppon <c.toppon@arts.ac.uk>
2010-12-22 20:52:47 +0000

    bugfix - path for icon/images has changed since url is now /ipad/

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 Ext.setup({
-    tabletStartupScreen: 'img/touch/tablet_startup.png',
+    tabletStartupScreen: '../img/touch/tablet_startup.png',
     phoneStartupScreen: 'phone_startup.png',
-    icon: 'img/touch/icon.png',
+    icon: '../img/touch/icon.png',
     glossOnIcon: true,
     onReady: function() {
 		Showtime.MasterPanel = new Showtime.MasterPanel();
